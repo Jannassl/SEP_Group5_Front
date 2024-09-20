423b764623c76b2cbee85b52a95db76ac1716e41
Jan koti PC <Jan.nassling@metropolia.fi>
2024-09-20 18:40:14 +0300

    Login sivu tehty

---
+++ src/main/java/Controller/LoginController.java
@@ -0,0 +1,20 @@
+package Controller;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.TextField;
+
+public class LoginController {
+
+    @FXML
+    private TextField loginPassword;
+
+    @FXML
+    private TextField loginUsername;
+
+    @FXML
+    void attemptLogin(ActionEvent event) {
+
+    }
+
+}
