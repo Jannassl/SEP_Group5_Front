3a0779fb98dbcb36196743c2d2fa71de1b31d175
Jan koti PC <Jan.nassling@metropolia.fi>
2024-09-21 21:38:10 +0300

    Mainmenu sivu tehty

---
+++ src/main/java/Controller/MainMenuController.java
@@ -0,0 +1,55 @@
+//
+// Source code recreated from a .class file by IntelliJ IDEA
+// (powered by FernFlower decompiler)
+//
+
+package controller;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.ListView;
+
+public class MainMenuController {
+    @FXML
+    private Button KalenteriButton;
+    @FXML
+    private Button KurssitButton;
+    @FXML
+    private ListView<?> ListViewMainMenu;
+    @FXML
+    private Button LogOutButton;
+    @FXML
+    private Button OppilaatButton;
+    @FXML
+    private Button ProfiiliButton;
+    @FXML
+    private Button TapahtumatButton;
+
+    public MainMenuController() {
+    }
+
+    @FXML
+    void CloseProgram(ActionEvent event) {
+    }
+
+    @FXML
+    void openKalenteriPage(ActionEvent event) {
+    }
+
+    @FXML
+    void openKurssitPage(ActionEvent event) {
+    }
+
+    @FXML
+    void openOppilaatPage(ActionEvent event) {
+    }
+
+    @FXML
+    void openProfiiliPage(ActionEvent event) {
+    }
+
+    @FXML
+    void openTapahtumatPage(ActionEvent event) {
+    }
+}
