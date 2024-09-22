6202bc3a68cac54ad51057c9ee8319bb12e35b7a
Jan koti PC <Jan.nassling@metropolia.fi>
2024-09-22 09:58:49 +0300

    Kirjaudu ulos nappi sulkee main menun ja avaa login ikkunan

--- src/main/java/controller/MainMenuController.java
+++ src/main/java/controller/MainMenuController.java
@@ -7,8 +7,14 @@ package controller;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.ListView;
+import javafx.stage.Stage;
+
+import java.io.IOException;
 
 public class MainMenuController {
     @FXML
@@ -31,6 +37,15 @@ public class MainMenuController {
 
     @FXML
     void CloseProgram(ActionEvent event) {
+        try {
+            FXMLLoader loader = new FXMLLoader(getClass().getResource("/login.fxml"));
+            Parent root = loader.load();
+            Stage stage = (Stage) ((Button) event.getSource()).getScene().getWindow();
+            stage.setScene(new Scene(root));
+            stage.show();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
     @FXML
