640b3519687f900b2924f677bf53e7457f7abd26
zzokku <93818329+zzokku@users.noreply.github.com>
2024-09-26 12:47:10 +0300

    Kurssi sivuun tekstikentät ja asettelu

--- src/main/java/controller/KurssitController.java
+++ src/main/java/controller/KurssitController.java
@@ -1,88 +1,81 @@
 package controller;
 
-import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import javafx.fxml.FXMLLoader;
-import javafx.scene.Parent;
-import javafx.scene.Scene;
 import javafx.scene.control.Button;
-import javafx.stage.Stage;
-
-import java.io.IOException;
+import javafx.scene.control.ListView;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
+import javafx.scene.text.Text;
 
 public class KurssitController {
-
-    @FXML
-    private Button LogOutButton;
-
-    @FXML
-    private Button CoursePageSearchButton;
 
     @FXML
     private Button ProfiiliButton;
 
     @FXML
+    private Button LogOutButton;
+
+    @FXML
+    private ListView<String> CourseListView;
+
+    @FXML
+    private TextField SearchTextField;
+
+    @FXML
+    private Button CoursePageSearchButton;
+
+    @FXML
     private Button TakaisinButton;
 
     @FXML
-    void CloseProgram(ActionEvent event) {
-        try {
-            FXMLLoader loader = new FXMLLoader(getClass().getResource("/login.fxml"));
-            Parent root = loader.load();
-            Stage stage = (Stage) ((Button) event.getSource()).getScene().getWindow();
-            stage.setScene(new Scene(root));
-            stage.show();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
+    private Text tunnusText;
+
+    @FXML
+    private Text nimiText;
+
+    @FXML
+    private Text opettajaText;
+
+    @FXML
+    private Text aloitusPvmText;
+
+    @FXML
+    private Text lopetusPvmText;
+
+    @FXML
+    private TextArea kuvausTextArea;
+
+    @FXML
+    private void initialize() {
+
     }
 
     @FXML
-    void navigateBackwards(ActionEvent event) {
-        try {
-            FXMLLoader loader = new FXMLLoader(getClass().getResource("/mainMenu.fxml"));
-            Parent root = loader.load();
-            Stage stage = (Stage) ((Button) event.getSource()).getScene().getWindow();
-            stage.setScene(new Scene(root));
-            stage.show();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
+    private void openProfiiliPage() {
+
     }
 
     @FXML
-    void openProfiiliPage(ActionEvent event) {
-        try {
-            FXMLLoader loader = new FXMLLoader(getClass().getResource("/profiili.fxml"));
-            Parent root = loader.load();
-            Stage stage = (Stage) ((Button) event.getSource()).getScene().getWindow();
-            stage.setScene(new Scene(root));
-            stage.show();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
+    private void CloseProgram() {
+
     }
 
     @FXML
-    void searchCourse(ActionEvent event) {
+    private void searchCourse() {
 
     }
 
+    @FXML
+    private void navigateBackwards() {
+
+    }
+
+    public void updateCourseInfo(String tunnus, String nimi, String opettaja, String aloitusPvm, String lopetusPvm, String kuvaus) {
+        tunnusText.setText(tunnus);
+        nimiText.setText(nimi);
+        opettajaText.setText(opettaja);
+        aloitusPvmText.setText(aloitusPvm);
+        lopetusPvmText.setText(lopetusPvm);
+        kuvausTextArea.setText(kuvaus);
+    }
 }
-
-
-/* NÄIN LISÄTÄÄN LISTVIEW KOHTAAN SISÄLTÖÄ
-    @FXML
-    private ListView<String> listViewMainMenu;
-
-    @FXML
-    public void initialize() {
-        // Create an observable list of items
-        ObservableList<String> items = FXCollections.observableArrayList(
-            "Item 1", "Item 2", "Item 3"
-        );
-
-        // Add items to the ListView
-        listViewMainMenu.setItems(items);
-    }
-     */
