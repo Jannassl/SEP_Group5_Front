6dae91927b0dc8e3cb8b8ee2cac75c8e4db94d22
Jannassl <Jan.nassling@metropolia.fi>
2024-09-30 11:27:55 +0300

    Studeninfopage oppilaat listan filtteröinti

--- src/main/java/controller/StudentInfoController.java
+++ src/main/java/controller/StudentInfoController.java
@@ -1,16 +1,16 @@
 package controller;
 
+import javafx.beans.value.ChangeListener;
+import javafx.beans.value.ObservableValue;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
+import javafx.collections.transformation.FilteredList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.control.Button;
-import javafx.scene.control.ListView;
-import javafx.scene.control.TableColumn;
-import javafx.scene.control.TableView;
+import javafx.scene.control.*;
 import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.stage.Stage;
 import model.KirjautunutKayttaja;
@@ -47,6 +47,10 @@ public class StudentInfoController {
     private TableColumn<Opiskelija, String> lastNameColumn;
 
     private OpiskelijaService opiskelijaService;
+    @FXML
+    private TextField SearchTextField;
+
+    private FilteredList<Opiskelija> filteredData;
 
     public StudentInfoController() {
         this.opiskelijaService = new OpiskelijaService();
@@ -59,6 +63,13 @@ public class StudentInfoController {
         lastNameColumn.setCellValueFactory(new PropertyValueFactory<>("sukunimi"));
 
         loadOpiskelijat();
+
+        // Create the FilteredList once
+        filteredData = new FilteredList<>(StudentTableView.getItems(), p -> true);
+        StudentTableView.setItems(filteredData);
+
+        // Add listener to SearchTextField
+        SearchTextField.textProperty().addListener((observable, oldValue, newValue) -> filterStudentList(newValue));
     }
 
     private void loadOpiskelijat() {
@@ -111,6 +122,20 @@ public class StudentInfoController {
     @FXML
     void searchOppilas(ActionEvent event) {
 
+    }
+    private void filterStudentList(String searchText) {
+        filteredData.setPredicate(opiskelija -> {
+            if (searchText == null || searchText.isEmpty()) {
+                return true;
+            }
+            String lowerCaseFilter = searchText.toLowerCase();
+            if (opiskelija.getEtunimi().toLowerCase().contains(lowerCaseFilter)) {
+                return true;
+            } else if (opiskelija.getSukunimi().toLowerCase().contains(lowerCaseFilter)) {
+                return true;
+            }
+            return false;
+        });
     }
 
 }
